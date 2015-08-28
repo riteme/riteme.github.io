271856907e7fa8654875794443a448b9a4786258
riteme <1412803389@qq.com>
2015-08-28 10:42:20 +0800

    test

--- strapdown/strapdown.js
+++ strapdown/strapdown.js
@@ -1374,18 +1374,6 @@ marked.options=marked.setOptions=function(opt){
         newNode.id = 'content';
         document.body.replaceChild(newNode, markdownEl);
 
-        // Insert navbar if there's none
-        // var newNode = document.createElement('div');
-        // newNode.className = 'navbar navbar-fixed-top';
-        // if (!navbarEl && titleEl) {
-        //     newNode.innerHTML = '<div class="navbar-inner"> <div class="container"> <div id="headline" class="brand"> </div> </div>
-        //     </div>';
-        //     document.body.insertBefore(newNode, document.body.firstChild);
-        //     var title = titleEl.innerHTML;
-        //     var headlineEl = document.getElementById('headline');
-        //     if (headlineEl) headlineEl.innerHTML = title;
-        // }
-
         //////////////////////////////////////////////////////////////////////
         //
         // Markdown!
